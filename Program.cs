1c8b123e1b2b196323065dd8fe040161f1a6d193
Yar Ffullex <72185613+Ffullex@users.noreply.github.com>
2021-03-29 15:55:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace LineExtractor_HCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            Console.WriteLine("Введите путь до файла");
+            string path = Console.ReadLine();
+            Console.WriteLine(path);
+            try
+            {
+                //Передайте путь к файлу и имя файла конструктору StreamReader.
+                StreamReader sr = new StreamReader(path);
+                //Прочтите первую строку текста
+                line = sr.ReadLine();
+                //Продолжайте читать, пока не дойдете до конца файла
+                while (line != null)
+                {
+                    //напишите ложь в окно консоли
+                    Console.WriteLine(line);
+                    //Прочтите следующую строку
+                    line = sr.ReadLine();
+                }
+                //закройте файл
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
