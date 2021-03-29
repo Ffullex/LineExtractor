d7fce07e3d0788defcf504930e7e2c1d8c770c50
Yar Ffullex <72185613+Ffullex@users.noreply.github.com>
2021-03-29 16:14:42 +0700

    some refactoring

--- Program.cs
+++ Program.cs
@@ -1,32 +1,36 @@
 ﻿using System;
 using System.IO;
 
+
 namespace LineExtractor_HCI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            String line;
-            Console.WriteLine("Введите путь до файла");
-            string path = Console.ReadLine();
-            Console.WriteLine(path);
             try
             {
+                
+                String line; //строка с блокнота
+                Console.WriteLine("Введите путь до файла (Пример: С:\\Sample.txt)");
+                string path = Console.ReadLine();
+                Console.WriteLine("Введите номера строк, которые необходимо достать");
+                string linesNum = Console.ReadLine();
+                //Console.WriteLine(path);
                 //Передайте путь к файлу и имя файла конструктору StreamReader.
-                StreamReader sr = new StreamReader(path);
+                var reader = new StreamReader(path);
                 //Прочтите первую строку текста
-                line = sr.ReadLine();
+                line = reader.ReadLine();
                 //Продолжайте читать, пока не дойдете до конца файла
                 while (line != null)
                 {
                     //напишите ложь в окно консоли
                     Console.WriteLine(line);
                     //Прочтите следующую строку
-                    line = sr.ReadLine();
+                    line = reader.ReadLine();
                 }
                 //закройте файл
-                sr.Close();
+                reader.Close();
                 Console.ReadLine();
             }
             catch (Exception e)
